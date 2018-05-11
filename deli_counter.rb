ba13e7e500ce530209de5cf9f0d11f6203b271d4
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 17:37:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,9 @@ end
 
 def take_a_number(katz_deli, new_customer)
   katz_deli << new_customer
-  puts "Welcome, #{new_customer}. You are number #{katz_deli.length + 1} in line."
-  end     
+    puts "Welcome, #{new_customer}. You are number #{katz_deli.length + 1} in line."
+    end   
+  end   
 end 
 
 def now_serving(next_person)
